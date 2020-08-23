5b279e268cf83065ad6b3d5ec334573dd3d76ee9
DBC201 <denizbcakiroglu@gmail.com>
2020-08-23 12:29:59 -0400

    initial commit

---
+++ git-pull.py
@@ -0,0 +1,84 @@
+import os
+import argparse, sys
+
+def pull(path):
+    print(f"Pulling \"{path}\"")
+    if os.system(f"git -C \"{path}\" pull origin master") == 0:
+        return True
+    else:
+        return False
+
+def initialize_repo(path):
+    print("Initializing git")
+    if os.system(f"git -C \"{path}\" init") == 0:
+        return True
+    else:
+        return False
+
+def remote_origin_add(path):
+    repo_name = path.split('\\')[-1].replace(' ', '-')
+    username = os.popen("git config user.name", "r").read().strip()
+    remote = f"https://github.com/{username}/{repo_name}"
+    print(f"Adding remote origin {remote}")
+    if os.system(f"git -C \"{path}\" remote add origin {remote}") == 0:
+        return True
+    else:
+        return False
+
+def remote_origin_remove(path):
+    print("Attempting to remote remove origin")
+    if os.system(f"git -C \"{path}\" remote remove origin") == 0:
+        print("Removed")
+        return True
+    else:
+        print("Removal error")
+        return False
+
+def push(path):
+    print("Attempting to push")
+    command = f"""cd \"{path}\"
+    git add .
+    git commit -m \"automated push\"
+    git push origin master"""
+    if os.system(command.replace('\n', '&')) == 0:
+        print("Push successful!")
+        return True
+    else:
+        print("Push failed")
+        return False
+
+def input_parse():
+    parser = argparse.ArgumentParser(description="Easy update for local and remote repos in a given directory")
+    parser.add_argument("-p", "--push", action="store_true", dest="push", help="Also attempts to push repos after attempting to pull")
+    parser.add_argument("-d", "--destination", nargs=1, dest="path", help="Full path to the directory program will run at")
+    return parser
+
+def main(argv):
+
+    parser = input_parse()
+    args = parser.parse_args(argv)
+    path = os.getcwd()
+
+    if args.path:
+        path = ''.join(args.path)
+    
+    try:
+        files = os.listdir(path)
+        files = [os.path.join(path, f) for f in files]
+    except Exception as e:
+        print(e)
+        sys.exit(-1)
+    
+    for f in files:
+        if os.path.isdir(f):
+            if not pull(f):
+                initialize_repo(f)
+                remote_origin_add(f)
+                if args.p
+                if not pull(f): # attempts to pull again after initializing and adding remote origin, removes the remote origin if failed
+                    remote_origin_remove(f)
+            if args.push:              
+                push(f)
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
---
+++ install.py
@@ -0,0 +1,213 @@
+import os
+import shutil
+
+def write_location(location):
+    user = os.path.expanduser("~")
+    install_folder = os.path.join(user, "Documents", "git-pull")
+    with open(os.path.join(install_folder, "location_log"), 'a') as file:
+        file.write(location + '\n')
+
+def return_locations():
+    user = os.path.expanduser("~")
+    install_folder = os.path.join(user, "Documents", "git-pull")
+    locations = []
+    with open(os.path.join(install_folder, "location_log"), 'r') as file:
+        while True:
+            line = file.readline().strip()
+            if not line:
+                break
+            locations.append(line)
+    return locations
+
+def remove_location(location):
+    locations = return_locations()
+    if location in locations:
+        locations.remove(location)
+        user = os.path.expanduser("~")
+        install_folder = os.path.join(user, "Documents", "git-pull")
+        with open(os.path.join(install_folder, 'location_log'), 'w') as file:
+            for location in locations:
+                file.write(location + '\n')
+
+def confirm_selection():
+    while True:
+        selection = input("Type confirm to confirm, return to change:").lower()
+        if selection == "confirm":
+            return True
+        elif selection == "return":
+            return False
+
+def yes_no_selection(prompt):
+    while True:
+        selection = input(f"{prompt}(y/n):").lower()
+        if selection == 'y':
+            return True
+        elif selection == 'n':
+            return False
+
+def get_repo_folder():
+    while True:
+        repo_folder = input("Enter the full path where your git repos are at:")
+        try:
+            repos = os.listdir(repo_folder)
+        except Exception as e:
+            print(e)
+            print("Failed to find or access the given folder")
+        else:
+            for repo in repos:
+                print(repo)
+            if confirm_selection():
+                return repo_folder
+
+def options():
+    menu = """
+    Type u to uninstall
+    Type r to change repo dir
+    Type s to add/remove from startup
+    Type c to create a bat file in desired dir
+    Type a to change arguments
+    Type e to exit
+    """
+    while True:
+        print(menu)
+        selection = input().lower()
+        user = os.path.expanduser("~")
+        if selection == 'u':
+            try:
+                for f in return_locations():
+                    try:
+                        os.remove(f)
+                    except Exception as e:
+                        print(e)
+                shutil.rmtree(os.path.join(user, "Documents\git-pull"))
+            except Exception as e:
+                print(e)
+                print("Error deleting files")
+            else:
+                print("Succesfully uninstalled!")
+                return
+        elif selection == 'r':
+            new_location = get_repo_folder()
+            for f in return_locations():
+                create_bat_file('\\'.join(f.split('\\')[:-1]), new_location)
+            print("Done!")
+        elif selection == 's':
+            startup_folder = os.path.join(
+                user,
+                "AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
+            )
+            if check_startup():
+                if yes_no_selection("Are you sure you want to remove the program from startup?"):
+                    try:
+                        os.remove(os.path.join(startup_folder, "run-git-pull.bat"))
+                    except Exception as e:
+                        print(e)
+                        print("Error removing from startup, perhaps run as admin?")
+                    else:
+                        remove_location(os.path.join(startup_folder, "run-git-pull.bat"))
+                        print("Sucessfully removed!")
+            else:
+                if yes_no_selection("Are you sure you want to add the program to startup?"):
+                    repo_folder = get_repo_folder()
+                    create_bat_file(startup_folder, repo_folder)
+                    print("Done!")
+        elif selection == 'c':
+            repo_folder = get_repo_folder()
+            folder = input("Enter the full path to desired directory:")
+            try:
+                create_bat_file(folder, repo_folder)
+            except OSError as e:
+                print(e)
+                print("Perhaps this dir doesn't exit or isn't accessible?")
+            else:
+                print("Done!")
+        elif selection == 'a':
+            print("This will change the arguments for all found .bat files")
+            push = yes_no_selection("Would you like to auto push after each pull?")
+            for f in return_locations():
+                repo_folder = ''
+                with open(f, "r") as file:
+                    content = file.read()
+                    if not push and content.find("-p") == -1:
+                        continue
+                    elif push and content.find("-p") != -1:
+                        continue
+                    index = content.find('-d')
+                    repo_folder = content[index+2:]
+                    repo_folder = repo_folder.strip(" \"")
+                remove_location(f)
+                os.remove(f)
+                if push:
+                    create_bat_file('\\'.join(f.split('\\')[:-1]), repo_folder, True)
+                else:
+                    create_bat_file('\\'.join(f.split('\\')[:-1]), repo_folder, False)
+            print("Done!")
+        elif selection == 'e':
+            return
+
+
+def check_startup():
+    return "run-git-pull.bat" in os.listdir("C:\\Users\\DBC\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup")
+
+def check_documents():
+    user = os.path.expanduser("~")
+    return "git-pull" in os.listdir(os.path.join(user, "Documents"))
+
+def create_bat_file(bat_location, script_destination, push=False):
+    script_path = os.path.join(
+        os.path.expanduser("~"),
+        "Documents",
+        "git-pull",
+        "git-pull.py"
+    )
+    push_argument = ''
+    if push:
+        push_argument = "-p"
+    path_argument = f"-d \"{script_destination}\""
+    command = f"""@echo off
+cmd /k \"python \"{script_path}\" {push_argument} {path_argument} \""""
+    with open(os.path.join(bat_location, "run-git-pull.bat"), "w") as file:
+        file.write(command)
+        write_location(os.path.join(bat_location, "run-git-pull.bat"))
+
+def install(repo_folder, startup):
+    user = os.path.expanduser("~")
+    install_folder = os.path.join(user, "Documents\git-pull")
+    try:
+        os.mkdir(install_folder)
+        shutil.copy2(
+            os.path.join(os.getcwd(), "git-pull.py"),
+            install_folder
+        )
+        create_bat_file(install_folder, repo_folder)
+        create_bat_file(os.path.join(user, "Desktop"), repo_folder)
+        if startup:
+            try:
+                create_bat_file(
+                    os.path.join("C:\\Users\\DBC\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup"), 
+                    repo_folder
+                )
+            except Exception as e:
+                print("Unable to add it to start, perhaps run as admin?")
+                print(e)        
+    except Exception as e:
+        print("Unable to install")
+        raise e
+    print("Install sucessful")
+    print("Run this again to view options")
+
+
+if __name__ == "__main__":
+    
+    print("git-pull by dbc201")
+    print("Press CTRL+C to halt")
+    if check_documents():
+        options()
+    elif check_startup():
+        print("Installation in startup found but not in documents")
+        print("Perhaps you would like to check it out?")
+        os.system("explorer.exe \"C:\\Users\\DBC\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\"")
+    else:
+        repo_folder = get_repo_folder()
+        startup = yes_no_selection("Would you like to have this program run on startup")
+        install(repo_folder, startup)   
